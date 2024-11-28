78f95b7fd0a5a34b43c654f7455ea74825f3849f
siluski <arsiluk@optonline.net>
2024-11-28 12:34:48 -0500

    message

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,50 @@
+let userRole="admin";
+let accessLevel;
+
+if (userRole === "admin"){
+    accessLevel = "Full Access Granted";
+}else if (userRole === "manager"){
+    accessLevel = "Limited Access Granted";
+}else{
+    accessLevel = "No access";
+}
+
+console.log("Access Level:", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if(isLoggedIn){
+    if(userRole === "admin"){
+        userMessage = "Welcome Admin";
+    }else{
+        userMessage = "Welcome User";
+    }
+}else{
+    useMessage = "Please login";
+}
+
+console.log("User Message:",userMessage);
+
+let userType= "subscriber";
+let userCategory;
+
+switch (userType){
+    case "admin":
+        userCategory="Administrator";
+        break;
+    case "manager":
+        userCategory="Manager";
+        break;
+    case "subscriber":
+        userCategory="Subscriber";
+        break;
+    default:
+        userCategory="Unknown";
+}
+
+console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not Authenticated";
+console.log("Authentication Status:", authenticationStatus);
