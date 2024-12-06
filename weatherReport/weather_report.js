93b5ce07a703e410647b4bd0245249d95aad6357
siluski <arsiluk@optonline.net>
2024-12-06 11:40:11 -0500

    added

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -3,9 +3,24 @@ document.getElementById('weatherForm').addEventListener('submit', showweatherDet
 function showweatherDetails(event){
     event.preventDefault();
     const city = document.getElementById('city').value;
+    const lat = document.getElementById('latitude').value;
+    const long = document.getElementById('longitude').value;
     const apiKey = 'bd72428ca5bf11cc37c9e0358bfad16a';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl;
+    if(city !== ''){
+        apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        getWeather(apiUrl);
+    }else if(lat !== ''&&long != ''){
+        console.log("getting weather via coordinate");
+        apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+        getWeather(apiUrl);
+    } 
 
+
+    
+}
+
+function getWeather(apiUrl){
     fetch(apiUrl).then(response => response.json()).then(data =>{
         const weatherInfo = document.getElementById('weatherInfo');
         weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
